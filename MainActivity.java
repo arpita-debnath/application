0276da8cac53d88b5d3e302d1c8bc1a50fe69c7b
arpita-debnath <51192957+arpita-debnath@users.noreply.github.com>
2019-06-06 22:23:24 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText fname, lname;
+    Button submit;
+    TextView fullname;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        fname = (EditText) findViewById(R.id.fname);
+        lname = (EditText) findViewById(R.id.lname);
+        submit = (Button) findViewById(R.id.submit);
+        fullname = (TextView) findViewById(R.id.fullname);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fname1 = fname.getText().toString();
+                String lname1 = lname.getText().toString();
+                if (TextUtils.isEmpty(fname1)) {
+                    fname.setError("Please enter name");
+                } else if (TextUtils.isEmpty(lname1)) {
+                    lname.setError("Enter Last Name");
+                } else {
+                    fullname.setText(fname1 + " " + lname1);
+                }
+            }
+        });
+    }
+}
